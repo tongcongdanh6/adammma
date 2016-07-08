f5ec3e2b0235cb45ca4daa61babde863de2288f4
tongcongdanh6 <tong.congdanh@gmail.com>
2016-07-08 12:13:14 +0700

    Delete all files

--- dangxuat.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("controllers/c_nguoi_dung.php");
-$c_nguoi_dung=new C_nguoi_dung();
-$c_nguoi_dung->Dang_Xuat();
-?>
--- introduction.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("controllers/c_nguoi_dung.php");
-include("controllers/c_introduction.php");
-$c_nguoi_dung=new C_nguoi_dung();
-$c_introduction=new C_Introduction();
-$c_nguoi_dung->Dang_Nhap();
-$c_introduction->HienThi_Introduction();
-?>
